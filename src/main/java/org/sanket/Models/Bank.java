c56139cb31918202f00e4b710a4c57b12bff0322
Bihart <sanket.bihari@gmail.com>
2022-11-29 20:13:52 -0500

    [update] add methods needed before java 14 (records)
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- src/main/java/org/sanket/Models/Bank.java
+++ src/main/java/org/sanket/Models/Bank.java
@@ -1,6 +1,8 @@
 package org.sanket.Models;
 
-public class Bank {
+import java.util.Objects;
+
+public class Bank implements Cloneable {
     private String noBank;
     private String name;
     private double maxMountByTransaction;
@@ -17,4 +19,22 @@ public class Bank {
         this.priceByTransaction = priceByTransaction;
         this.interestByYear = interestByYear;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bank bank = (Bank) o;
+        return Double.compare(bank.maxMountByTransaction, maxMountByTransaction) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(noBank);
+    }
+
+    public Bank clone() throws CloneNotSupportedException {
+        return (Bank) super.clone();
+    }
+
 }
